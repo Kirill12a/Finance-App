9bddd930105e4468d7d405f4a9d6c3502079ca2a
Kirill12a <kirilldrozd12@gmail.com>
2021-05-09 22:39:35 +0300

    Initial Commit

---
+++ Finance App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Finance App
+//
+//  Created by Kirill Drozdov on 09.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
