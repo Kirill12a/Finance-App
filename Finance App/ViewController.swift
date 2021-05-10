1115d83d442503558f5b93b6400a49732278a4da
Kirill12a <kirilldrozd12@gmail.com>
2021-05-10 08:24:16 +0300

    Начинаю делать добавление категории расходов

--- Finance App/ViewController.swift
+++ Finance App/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var fas = ["fa","fa","fsa"]
 
+    @IBOutlet weak var TBMain: UITableView!
+    @IBOutlet weak var AddNewCategoryOutlet: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        TBMain.delegate = self
+        TBMain.dataSource = self
+        editButtonAddCategory()
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return fas.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = TBMain.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = fas[indexPath.row]
+            return cell
+        }
 
-
+    @IBAction func AddNewCategory(_ sender: Any) {
+    }
+    func editButtonAddCategory(){
+        AddNewCategoryOutlet.layer.cornerRadius = 20
+    }
 }
 
